72ef4a33660c0ad506167c85dd46cab9f684e826
Justin Smith <justinasmith662@gmail.com>
2021-02-28 13:29:44 -0600

    Add Retirement class without functionality

---
+++ src/Retirement.js
@@ -0,0 +1,52 @@
+class Retirement {
+
+    constructor() {
+        this.age = NaN;
+        this.salary = 0;
+        this.percentSaved = 0;
+        this.savingGoal = 0;
+        this.employerMatch = 1.35; // 35% match
+    }
+
+    getAgeGoalMet() {
+        return;
+    }
+
+    getSavingPerYear() {
+        return;
+    }
+
+    getYearsTilGoal() {
+        return;
+    }
+
+    setAge(age) {
+        return;
+    }
+
+    getAge() {
+        return;
+    }
+
+    setSalary(salary) {
+        return;
+    }
+
+    getSalary() {
+        return;
+    }
+
+    setPercentSaved(percent) {
+        return;
+    }
+
+    getPercentSaved() {
+        return;
+    }
+
+    setSavingGoal(goal) {
+        return;
+    }
+}
+
+exports.Retirement = Retirement;
