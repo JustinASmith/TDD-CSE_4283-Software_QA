1d65135bf252ebb1f48fc9b46b2c18ad48645c9e
Justin Smith <justinasmith662@gmail.com>
2021-02-28 00:03:27 -0600

    Start adding user interface and connecto to tested code

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 const { BMI } = require("./BMI");
+
+const bmi = new BMI();
+
+try {
+  function handleFeet() {
+    const feet = document.getElementById('feet');
+    bmi.setFeet(feet.value);
+  }
+
+  function handleInches() {
+    const inches = document.getElementById('inches');
+    bmi.setInches(inches.value);
+  }
+
+  function handlePounds() {
+    const pounds = document.getElementById('pounds');
+    bmi.setPounds(pounds.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(bmi.getBMI());
+  }
+
+  window.handleFeet = handleFeet;
+  window.handleInches = handleInches;
+  window.handlePounds = handlePounds;
+  window.handleSubmit = handleSubmit;
+} catch(err) {}
